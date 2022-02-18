ca3a3c19af42dfb2e873f8e436983c9b093c2ce4
taeheon.song <taeheon.song@linecorp.com>
2022-02-18 21:49:11 +0900

    [SONG-1] init batch project

---
+++ src/test/java/com/song/study/batch/batchtest/BatchtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.study.batch.batchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
